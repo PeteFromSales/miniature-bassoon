8f76495add2b3c8c4c19bd9f158be9400f96e89e
PeteFromSales <jordan@truefacet.com>
2017-12-01 13:58:44 -0500

    Build script wrong

--- scripts/ec2-launch.sh
+++ scripts/ec2-launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-REGION$=(curl 169.254.169.254/latest/meta-data/placement/availability-zone/ | sed 's/[a-z]$//')
+REGION=$(curl 169.254.169.254/latest/meta-data/placement/availability-zone/ | sed 's/[a-z]$//')
 
 sudo apt update && sudo apt -y upgrade
 sudo apt install -y ruby python vim nginx php70
