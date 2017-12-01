c54b9f67e079abd3bf16688526ae426980c50743
PeteFromSales <jordan@truefacet.com>
2017-12-01 11:09:06 -0500

    EC2 (userdata) script added

---
+++ scripts/ec2-launch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+REGION$=(curl 169.254.169.254/latest/meta-data/placement/availability-zone/ | sed 's/[a-z]$//')
+
+sudo apt update && sudo apt -y upgrade
+sudo apt install -y ruby python vim nginx php70
+cd /home/ubuntu
+wget https://aws-codedeploy-$REGION.s3.amazonaws.com/latest/install
+chmod +x ./install
+./install auto
